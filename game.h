0fef565860aa46d4e3719df39091a296b261253b
Vlados <gendalf550mavric@gmail.com>
2020-01-11 18:28:13 +0200

    The Game class was described

---
+++ game.h
@@ -0,0 +1,30 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <utility>
+#include "field.h"
+
+class Player;
+
+class Game
+{
+public:
+	Game(Player *p1, Player *p2);
+	void Play();
+	bool IsEnded();
+	Player *GetCurrPlayer() const ;
+private:
+	std::size_t winLength = 5;
+	void UpdateStatus(FieldSetPosition fpos);
+	void UpdatePlayer();
+	bool gameEnded;
+	bool currPlayer;
+	bool CheckRow(const size_t x,const  size_t y, const vvc &field);
+	bool CheckCow(const size_t x, const size_t y, const vvc &field);
+	bool CheckMajorDiagonal(const size_t x, const size_t y, const vvc &field);
+	bool CheckMinorDiagonal(const size_t x, const size_t y, const vvc &field);
+	std::pair<Player*, Player*> players;
+	FieldSet fSet;
+};
+
+#endif
