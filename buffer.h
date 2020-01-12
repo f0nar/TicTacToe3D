1939f6747c923ee64d3a1972cb0b5143cbd92c21
Vlados <gendalf550mavric@gmail.com>
2020-01-12 20:46:38 +0200

    The Buffer interface declaration was changed

--- buffer.h
+++ buffer.h
@@ -1,6 +1,9 @@
 #ifndef BUFFER_H
 #define BUFFER_H
 
+#include <vector>
+#include <string>
+
 class FieldSet;
 struct FieldSetPosition;
 
@@ -9,6 +12,9 @@ class Buffer
 public:
     virtual FieldSetPosition ReadFieldPos() = 0;
     virtual void DrawField(const FieldSet &fSet) = 0;
+    virtual void DrawField(const FieldSet &fSet, std::string msg) = 0;
+    virtual void PrintMessage(const std::string msg) = 0;
+    virtual std::size_t ChooseOneFromList(const std::vector<std::string> &optList, std::size_t count) = 0;
 };
 
 #endif
